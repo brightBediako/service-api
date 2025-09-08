9b527771e616bd13ea96e9c476b03b0e6878931e
Bright Bediako <bright.bediako.dev@gmail.com>
2025-09-08 07:04:33 +0000

    modified files

--- app/app.js
+++ app/app.js
@@ -1,35 +1,48 @@
-import dotenv from "dotenv";
-import cors from "cors";
-
-dotenv.config();
 import express from "express";
+import dotenv from "dotenv";
+
+
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import dbConnect from "../config/dbConnect.js";
 import { globalErrhandler, notFound } from "../middlewares/globalErrHandler.js";
 
 
+dotenv.config();
 //db connect
 dbConnect();
 const app = express();
+
 //cors
 app.use(cors());
 
 // pass incoming data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// serve static files
-// app.use(express.static("public"));
-// home route
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join("public", "index.html"));
-// });
+app.use(cookieParser());
 
 
 // custom routes
-// app.use("/api/v1/users", usersRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 
 // error middleware
 app.use(notFound);
 app.use(globalErrhandler);
+
 export default app;
