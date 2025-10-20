a4bcf8a014c9c01ffaa13904763bd8cfdb2a498f
Bright Bediako <bright.bediako.dev@gmail.com>
2025-10-20 12:21:09 +0000

    modify files

--- app/app.js
+++ app/app.js
@@ -29,8 +29,6 @@ const corsOptions = {
 
     const allowedOrigins = [
       "https://service-two-sand.vercel.app",
-      "http://localhost:8000",
-      "http://127.0.0.1:8000/",
       "http://localhost:5173",
       "http://localhost:3000",
       "http://127.0.0.1:5173",
